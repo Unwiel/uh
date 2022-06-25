f14df0c2b84d609586d7777f7c999eb18f2e5324
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-06-25 17:51:57 -0500

    Update Add Black Camera Flash.lua

--- assets/preload/custom_events/Add Black Camera Flash.lua
+++ assets/preload/custom_events/Add Black Camera Flash.lua
@@ -1,5 +1,5 @@
 function onEvent(name, value1, value2)
     if name == 'Add Black Camera Flash' then
-        cameraFlash('other', '000000', 1.5, true)
+        cameraFlash('other', '000000', 1, true)
     end
 end 
