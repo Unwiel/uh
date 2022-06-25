cb15b5ae924a8befef222e245d200af14b2273d8
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-06-25 01:05:07 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -151,7 +151,7 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(20 - , 100 - (i * 120));
+			var menuItem:FlxSprite = new FlxSprite(20, 100 - (i * 120));
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
@@ -166,7 +166,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.updateHitbox();
 		}
 
-        var bsid:FlxText = new FlxText(12, FlxG.height - 04, 0, "BSIDES CORRUPTION DEMO v1.3.0", 12);
+                var bsid:FlxText = new FlxText(12, FlxG.height - 4, 0, "BSIDES CORRUPTION DEMO v1.3.0", 12);
 		bsid.scrollFactor.set();
 		bsid.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(bsid);
